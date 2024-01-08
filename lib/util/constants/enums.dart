40f35921dc300883e72ba21a8d33818706a02436
theghostmac <theghostmac@gmail.com>
2024-01-08 23:05:48 +0100

    [feature] more constants for transactions

---
+++ lib/util/constants/enums.dart
@@ -0,0 +1,5 @@
+enum TextSizes { small, medium, large}
+
+enum OrderStatus { processing, shipped, delivered }
+
+enum PaymentMethods { masterCard, crypto, bankTransfer }
---
+++ lib/util/constants/texts.dart
@@ -0,0 +1,16 @@
+class EcommerceTexts {
+  // - Global texts
+
+  // - Onboarding texts
+  static const String onboardingTitle1 = "Choose your product";
+  static const String onboardingTitle2 = "Select Payment Method";
+  static const String onboardingTitle3 = "Deliver at your door step";
+
+  static const String onboardingSubTitle1 = "Welcome to a World of Limitless Choices - Your Perfect Product Awaits!";
+  static const String onboardingSubTitle2 = "For Seamless Transactions, Choose Your Payment Path - Your Convenience, Our Priority!";
+  static const String onboardingSubTitle3 = "From Our Doorstep to Yours - Swift, Secure, and Contactless Delivery!";
+
+  // Home.
+  static const String homeAppBarTitle = "Good day for shopping";
+  static const String homeAppBarSubTitle = "Udoh Peace";
+}
