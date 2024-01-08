ff525ba53a44224856915224d264246584b3f119
theghostmac <theghostmac@gmail.com>
2024-01-09 00:00:46 +0100

    [feature] created the formatter for phone number and the nigerian naira

--- lib/util/formatters/formatter.dart
+++ lib/util/formatters/formatter.dart
@@ -1,3 +1,37 @@
 import 'package:intl/intl.dart';
 
-class C
+class ECommerceFormatter {
+  static String formatDate(DateTime? date) {
+    date ??= DateTime.now();
+    return DateFormat('dd-MM-yyyy').format(date);
+  }
+
+  static String formatCurrency(double amount) {
+    return NumberFormat.currency(
+      locale: 'en_NG',
+      symbol: '₦', // Nigerian Naira symbol
+      decimalDigits: 2,
+      name: "Naira",
+    ).format(amount);
+  }
+
+  static String formatPhoneNumber(String phoneNumber) {
+    // Regular expression for Nigerian phone number format (+234 800 1234 5678)
+    RegExp phoneRegex = RegExp(r'^(?:\+234|0)(\d{3})(\d{4})(\d{4})$');
+
+    if (phoneRegex.hasMatch(phoneNumber)) {
+      return '+234 ${phoneRegex.firstMatch(phoneNumber)!.group(1)} ${phoneRegex.firstMatch(phoneNumber)!.group(2)} ${phoneRegex.firstMatch(phoneNumber)!.group(3)}';
+    } else {
+      // Handle invalid phone number format
+      return phoneNumber; // Or throw an error if desired
+    }
+  }
+
+  static String formatInternationalCurrency(double amount, String currencyCode) {
+    return NumberFormat.currency(
+      locale: currencyCode, // Set locale based on currency code
+      symbol: NumberFormat.simpleCurrency(locale: currencyCode).currencySymbol,
+      decimalDigits: 2, // Display two decimal places
+    ).format(amount);
+  }
+}
