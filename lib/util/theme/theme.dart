2274ef65cb2b0525b61857f6dbda878c72e3395c
theghostmac <theghostmac@gmail.com>
2024-01-08 21:41:06 +0100

    [feature] completed all custom themes for theme data

--- lib/util/theme/theme.dart
+++ lib/util/theme/theme.dart
@@ -1,5 +1,10 @@
+import 'package:ecommerce_app/util/theme/custom_themes/bottom_sheet_theme.dart';
+import 'package:ecommerce_app/util/theme/custom_themes/checkbox_theme.dart';
 import 'package:ecommerce_app/util/theme/custom_themes/elevated_button_theme.dart';
+import 'package:ecommerce_app/util/theme/custom_themes/outlined_button_theme.dart';
 import 'package:ecommerce_app/util/theme/custom_themes/text_theme.dart';
+import 'package:ecommerce_app/util/theme/custom_themes/appbar_theme.dart';
+import 'package:ecommerce_app/util/theme/custom_themes/textfield_theme.dart';
 import 'package:flutter/material.dart';
 
 class EcommerceAppTheme {
@@ -13,6 +18,11 @@ class EcommerceAppTheme {
     scaffoldBackgroundColor: Colors.white,
     textTheme: EcommerceTextTheme.lightTextTheme,
     elevatedButtonTheme: EcommerceElevatedButtonTheme.lightElevatedButton,
+    appBarTheme: EcommerceAppBarTheme.lightAppBarTheme,
+    bottomSheetTheme: EcommerceBottomSheetTheme.lightBottomSheetTheme,
+    inputDecorationTheme: EcommerceTextFieldTheme.lightInputDecorationTheme,
+    outlinedButtonTheme: EcommerceOutlinedButtomTheme.lightOutlinedButtonTheme,
+    checkboxTheme: EcommerceCheckBoxTheme.lightCheckBoxTheme,
   );
 
   static ThemeData darkTheme = ThemeData(
@@ -23,5 +33,10 @@ class EcommerceAppTheme {
     scaffoldBackgroundColor: Colors.black,
     textTheme: EcommerceTextTheme.darkTextTheme,
     elevatedButtonTheme: EcommerceElevatedButtonTheme.darkElevatedButton,
+    appBarTheme: EcommerceAppBarTheme.darkAppBarTheme,
+    bottomSheetTheme: EcommerceBottomSheetTheme.darkBottomSheetTheme,
+    inputDecorationTheme: EcommerceTextFieldTheme.darkInputDecorationTheme,
+    outlinedButtonTheme: EcommerceOutlinedButtomTheme.darkOutlinedButtonTheme,
+    checkboxTheme: EcommerceCheckBoxTheme.darkCheckBoxTheme,
   );
 }
