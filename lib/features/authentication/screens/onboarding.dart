92db28dccd31079718bc8691499baf7c9767a473
theghostmac <theghostmac@gmail.com>
2024-01-10 21:23:28 +0100

    [feature] creating first onboarding screeen

--- lib/features/authentication/screens/onboarding.dart
+++ lib/features/authentication/screens/onboarding.dart
@@ -1,3 +1,6 @@
+import 'package:ecommerce_app/util/constants/image_strings.dart';
+import 'package:ecommerce_app/util/constants/texts.dart';
+import 'package:ecommerce_app/util/helpers/helper_functions.dart';
 import 'package:flutter/material.dart';
 
 class OnboardingScreen extends StatelessWidget {
@@ -5,7 +8,33 @@ class OnboardingScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      body: Stack(
+        children: ([
+          // Horizontal scrollable pages
+          PageView(
+            children: [
+              Column(
+                children: [
+                  Image(
+                    image: const AssetImage(EcommerceImages.onboardingImage1),
+                    width: EcommerceHelperFunctions.screenWidth() * 0.8,
+                    height: EcommerceHelperFunctions.screenWidth() * 0.6,
+                  ),
+                  Text(
+                    EcommerceTexts.onboardingTitle1, style: ,)
+                ],
+              )
+            ],
+          )
+
+          // Skip button
+
+          // Dot Navigation smooth page indicator
+
+          // circular button
+        ]),
+      ),
+    );
   }
 }
-
--- lib/util/constants/image_strings.dart
+++ lib/util/constants/image_strings.dart
@@ -7,4 +7,9 @@ class EcommerceImages {
   // -- Social Logos
   static const String google = "assets/icons/google-icon.png";
   static const String facebook = "assets/icons/facebook-icon.png";
+
+  // -- Onboarding images.
+  static const String onboardingImage1 = "assets/images/on_boarding_images/sammy-line-searching.gif";
+  static const String onboardingImage2 = "assets/images/on_boarding_images/sammy-line-shopping.gif";
+  static const String onboardingImage3 = "assets/images/on_boarding_images/sammy-line-delivery.gif";
 }
