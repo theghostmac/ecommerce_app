f2a21aa501c645a0b2e4258495cbba0079fbd80b
theghostmac <theghostmac@gmail.com>
2024-01-11 15:26:30 +0100

    [feature] onboarding images creation

--- lib/app.dart
+++ lib/app.dart
@@ -1,15 +1,18 @@
+import 'package:ecommerce_app/features/authentication/screens/onboarding.dart';
 import 'package:ecommerce_app/util/theme/theme.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
 class EcommerceApp extends StatelessWidget {
   const EcommerceApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return GetMaterialApp(
       themeMode: ThemeMode.system,
       theme: EcommerceAppTheme.lightTheme,
       darkTheme: EcommerceAppTheme.darkTheme,
+      home: const OnboardingScreen(),
     );
   }
 }
--- lib/features/authentication/screens/onboarding.dart
+++ lib/features/authentication/screens/onboarding.dart
@@ -1,4 +1,5 @@
 import 'package:ecommerce_app/util/constants/image_strings.dart';
+import 'package:ecommerce_app/util/constants/sizes.dart';
 import 'package:ecommerce_app/util/constants/texts.dart';
 import 'package:ecommerce_app/util/helpers/helper_functions.dart';
 import 'package:flutter/material.dart';
@@ -19,10 +20,21 @@ class OnboardingScreen extends StatelessWidget {
                   Image(
                     image: const AssetImage(EcommerceImages.onboardingImage1),
                     width: EcommerceHelperFunctions.screenWidth() * 0.8,
-                    height: EcommerceHelperFunctions.screenWidth() * 0.6,
+                    height: EcommerceHelperFunctions.screenHeight() * 0.6,
                   ),
                   Text(
-                    EcommerceTexts.onboardingTitle1, style: ,)
+                    EcommerceTexts.onboardingTitle1,
+                    style: Theme.of(context).textTheme.headlineMedium,
+                    textAlign: TextAlign.center,
+                  ),
+                  const SizedBox(
+                    height: EcommerceSizes.spaceBetweenItems,
+                  ),
+                  Text(
+                    EcommerceTexts.onboardingSubTitle1,
+                    style: Theme.of(context).textTheme.bodyMedium,
+                    textAlign: TextAlign.center,
+                  ),
                 ],
               )
             ],
